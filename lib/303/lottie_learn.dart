b7923afa37bee8a5dcc651bcad1d03deff375c63
pinareray <pinareray3@gmail.com>
2025-05-28 16:01:02 +0300

    added navigator file and lottie

---
+++ lib/303/lottie_learn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LottieLearn extends StatefulWidget {
+  const LottieLearn({super.key});
+
+  @override
+  State<LottieLearn> createState() => _LottieLearnState();
+}
+
+class _LottieLearnState extends State<LottieLearn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
